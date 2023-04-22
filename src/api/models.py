226659075a910254040189a0a7dbc6f68368859f
Alejandro Leon <alejandroleortiz57@gmail.com>
2023-04-22 13:07:46 -0400

    finalizadas tablas

--- src/api/models.py
+++ src/api/models.py
@@ -13,6 +13,8 @@ class User(db.Model):
     profile_picture = db.Column(db.String(150), nullable=False)
     roles_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable =False)
     roles = db.relationship('Role',  backref="user")
+    medical_record = db.relationship('Medical_record',  backref="user", uselist = False)
+    medical_appoinment = db.relationship('Medical_appoinment',  backref="user")
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -31,12 +33,36 @@ class Role(db.Model):
 
 class Medical_file(db.Model):
     __tablename__ = 'medical_file'
+    id = db.Column(db.Integer, primary_key=True)
     user_id=db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
     file = db.Column(db.String(150), nullable=False)
     date = db.Column(db.DateTime(), default=db.func.now(), nullable=False)
 
 class Medical_record(db.Model):
     __tablename__ = 'medical_record'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id'))
+    full_name = db.Column(db.String(150), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    identity_card: db.Column(db.Integer, nullable=False)
+    adress: db.Column(db.String(200), nullable=False)
+    phone_number:db.Column(db.Integer, nullable=False)
+    reason_for_consultation: db.Column(db.String(200), nullable=False)
+    current_illness: db.Column(db.String(200), nullable=False)
+    criminal_record: db.Column(db.String(200), nullable=False)
+    family_history: db.Column(db.String(200), nullable=False)
+    surgical_history: db.Column(db.String(200), nullable=False)
+    physical_examination: db.Column(db.String(500), nullable=False)
+    diagnosis: db.Column(db.String(200), nullable=False)
+    treatment: db.Column(db.String(200), nullable=False)
+    observations: db.Column(db.String(200), nullable=False)
+    date = db.Column(db.DateTime(), default=db.func.now(), nullable=False)
+
+class Medical_appoinment(db.Model):
+    __tablename__ = 'medical_appoinment'
+    id = db.Column(db.Integer, primary_key=True)
     user_id=db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    
+    doctor_id=db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    file = db.Column(db.String(150), nullable=False)
+    comment = db.Column(db.String(500), nullable=False)
     date = db.Column(db.DateTime(), default=db.func.now(), nullable=False)
